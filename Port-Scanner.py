21d84f9ff262f6d949877492ad2617032852f748
Robin <robincdj2009@foxmail.com>
2015-05-05 21:05:36 +0800

    update

---
+++ Port-Scanner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+import socket
+ports = [21, 22, 53, 445, 80, 443, 3389, 8080]
+hosts = ['127.0.0.1', '10.10.10.10', '192.168.1.1']
+for host in hosts:
+    for port in ports:
+        try:
+            s = socket.socket()
+            print "[+] Attempting to connect to " + host + ":" + str(port)
+            s.connect((host, port))
+            s.send('hello bird! /n')
+            banner = s.recv(1024)
+            if banner:
+                print "[+] " + host + ":" + str(port) + " open: \n" + banner
+            s.close()
+        except:
+            pass
